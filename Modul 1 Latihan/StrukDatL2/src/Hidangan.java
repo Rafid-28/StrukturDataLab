4a1cae78ccedff588b3ba74f3bb4a2000fd075a7
Muhammad Rafid <96788115+Rafid-28@users.noreply.github.com>
2023-03-08 20:16:55 +0700

    Add files via upload

---
+++ Modul 1 Latihan/StrukDatL2/src/Hidangan.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class Hidangan {
+    protected String namaHidangan;
+    
+    public String getNamaHidangan(){
+        return namaHidangan;
+    }
+    
+    public void setNamaHidangan(String namaHidangan){
+        this.namaHidangan = namaHidangan;
+    }
+    
+    public String disantap(){
+        return " Makananan Dihidangkan";
+    }
+}
